938a9255b5151b4db699675aa3f6942f9cea3c6a
mrsimonsen <=>
2020-11-19 15:27:11 -0700

    started lesson10

---
+++ lesson10/ReadIt.java
@@ -0,0 +1,47 @@
+/*Read It
+Demonstrates reading from a text file*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadIt{
+
+	public static void main(String[] args){
+		Scanner reader;
+		File file;
+		String f = "read_it.txt"; //file name
+
+		System.out.println("Opening and closing the file.");
+		try{
+			file = new File(f);
+			reader = new Scanner(file);
+			reader.close();
+
+			System.out.println("\nReading words from a line.");
+			reader = new Scanner(file);
+			System.out.println(reader.next());
+			System.out.println(reader.next());
+			reader.close();
+
+			System.out.println("\nReading one line at a time.");
+			reader = new Scanner(file);
+			System.out.println(reader.nextLine());
+			System.out.println(reader.nextLine());
+			reader.close();
+
+			System.out.println("\nReading the entire file at once.");
+			reader = new Scanner(file);
+			String text = reader.nextLine();
+			while(reader.hasNextLine()){
+				text += "\n"+reader.nextLine();
+			}
+			System.out.println(text);
+			reader.close();
+		}
+		catch(FileNotFoundException e){
+			System.out.println("Can't find"+f);
+		}
+	}
+}
+
