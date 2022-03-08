57f790b243db6153cd15e5c9e65248e3030e1123
mrsimonsen <johnsimonsen@weber.edu>
2022-03-08 13:45:49 -0700

    more efficient char response checking

--- lesson07/ReceiveAndReturn2.java
+++ lesson07/ReceiveAndReturn2.java
@@ -6,17 +6,10 @@ import java.util.Scanner;
 public class ReceiveAndReturn2{
 	public static char askYesNo(String question, Scanner scnr){
 		char response = 'a';
-		char[] possible  = {'y','n'};
-		boolean accepted = false;
-		while(!accepted){
+		String possible  = "yn";
+		while(possible.indexOf(response) == -1){
 			System.out.println(question);
 			response = scnr.nextLine().charAt(0);
-			for(char r:possible){
-				if (response==r){
-					accepted = true;
-					break;
-				}
-			}
 		}
 		return response;
 	}
@@ -40,11 +33,10 @@ public class ReceiveAndReturn2{
 
 	public static void main(String[] args){
 		Scanner scnr = new Scanner(System.in);
-		
+
 		char a = askYesNo("\nPlease enter 'y' or 'n':",scnr);
 		System.out.println("Thanks for entering: "+a);
 		String answer = askYesNo(scnr);
 		System.out.println("Thanks for entering: "+answer);
 	}
 }
-
