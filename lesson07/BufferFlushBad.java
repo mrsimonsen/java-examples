fa4c8504852dc690ad45ee547cccda590c860b02
mrsimonsen <johnsimonsen@weber.edu>
2022-03-08 14:51:49 -0700

    created buffer flush examples

---
+++ lesson07/BufferFlushBad.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BufferFlushBad(){
+	public static void main(String[] args){
+		Scanner scnr = new Scanner("1\n2.4\nC\nString\n");
+
+		System.out.println(scnr.nextInt());
+		//should be 1
+		System.out.println(scnr.nextDouble());
+		//should be 2.4
+		System.out.println(scnr.next().charAt(0));
+		//should be C
+		System.out.println(scnr.nextLine());
+		//should be String
+	}
+}
---
+++ lesson07/BufferFlushGood.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BufferFlushGood(){
+	public static void main(String[] args){
+		Scanner scnr = new Scanner("1\n2.4\nC\nString\n");
+
+		System.out.println(scnr.nextInt());//should be 1
+		System.out.println(scnr.nextDouble());//should be 2.4
+		System.out.println(scnr.next().charAt(0));//should be C
+		scnr.nextLine();//flush buffer
+		System.out.println(scnr.nextLine());//flush buffer
+	}
+}
